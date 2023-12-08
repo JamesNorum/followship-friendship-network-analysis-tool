877785f60b40dc5fa8b0cca0aa18d6c2849c8fa3
JamesNorum <70328051+JamesNorum@users.noreply.github.com>
2023-12-07 20:40:08 -0600

    Updated Graph Layout in new interactive implementation

--- targets_of_follower.py
+++ targets_of_follower.py
@@ -1,8 +1,11 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import networkx as nx
 import streamlit as st
-import plotly.graph_objects as go
+from pyvis.network import Network
+import streamlit.components.v1 as components
+import tempfile
+import os
+import shutil
 
 def get_targets_user_follows(follower, df):
     df_targets_user_follows = df[df.Follower == follower]
@@ -10,67 +13,74 @@ def get_targets_user_follows(follower, df):
 
 def visualize_targets_user_follows(target, df):
     G = nx.from_pandas_edgelist(df, 'Follower', 'Target', create_using=nx.DiGraph())
+    
+    pos = nx.circular_layout(G)
+    
+    # Create a Pyvis network
+    net = Network(notebook=False, width="100%", height="900px", directed=True)
+    net.toggle_hide_edges_on_drag(False)
+    net.toggle_physics(True)
 
-    pos = nx.spring_layout(G)
+    for node in G.nodes:
+        size = 35 if node == target else 25  # Increase the size for better visibility
+        color = '#FF9999' if node == target else '#4169E1'  # Light blue color for regular nodes
+        border_color = '#00008B'  # Dark blue border for contrast
+        title = f"ID: {node}"  # The title will be shown on hover
+        net.add_node(node, label=str(node), x = pos[node][0]*1000, y=pos[node][1]*1000,size=size, color=color, title=title, borderWidth=2, borderColor=border_color, font={'size': 14, 'color': '#000000'})
 
-    edge_traces = []
-    for edge in G.edges():
-        x0, y0 = pos[edge[0]]
-        x1, y1 = pos[edge[1]]
-        edge_traces.append(go.Scatter(x=[x0, x1, None], y=[y0, y1, None],
-                                      mode='lines',
-                                      line=dict(width=1, color='rgba(50,50,50,0.5)'), # Make lines darker
-                                      hoverinfo='none'))
+    for edge in G.edges:
+        net.add_edge(edge[0], edge[1], width=0.5)  # Keep edges thin
 
-    node_x = [pos[node][0] for node in G.nodes()]
-    node_y = [pos[node][1] for node in G.nodes()]
+    # Use hierarchical layout to potentially improve the clarity
+    net.set_options("""
+    {
+      "physics": {
+        "hierarchicalRepulsion": {
+          "centralGravity": 0.0,
+          "springLength": 100,
+          "springConstant": 0.01,
+          "nodeDistance": 120,
+          "damping": 0.09
+        },
+        "minVelocity": 0.75,
+        "solver": "hierarchicalRepulsion"
+      },
+      "nodes": {
+        "scaling": {
+          "label": {
+            "enabled": true
+          }
+        }
+      },
+      "edges": {
+        "color": {
+          "inherit": true
+        },
+        "smooth": false
+      },
+      "layout": {
+        "hierarchical": {
+          "enabled": false,
+          "levelSeparation": 150,
+          "nodeSpacing": 100,
+          "treeSpacing": 200,
+          "blockShifting": true,
+          "edgeMinimization": true,
+          "parentCentralization": true,
+          "direction": "DU", 
+          "sortMethod": "directed" 
+        }
+      }
+    }
+    """)
 
-    node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode='markers',
-        hoverinfo='text',
-        marker=dict(
-            size=[50 if node == target else 25 for node in G.nodes()], # Larger size for the target node
-            color=[1 if node == target else 0 for node in G.nodes()], # Different color for the target node
-            colorscale=['#888', '#888'], # Grayscale colors
-            line=dict(width=2, color='DarkSlateGrey')))
+    # Create a temporary file
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.html')
+    
+    # Generate the network in the temporary file
+    net.save_graph(temp_file.name)
 
-    # Set hover text to node IDs
-    node_text = [f'ID: {node}' for node in G.nodes()]
-    node_trace.text = node_text
-
-    fig = go.Figure(data=edge_traces + [node_trace],
-                    layout=go.Layout(
-                        title=f"Targets that User with ID: {target} follows",
-                        showlegend=False,
-                        hovermode='closest',
-                        margin=dict(b=20,l=5,r=5,t=40),
-                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
-
-    # Adding arrow annotations for directed edges
-    for edge in G.edges():
-        x0, y0 = pos[edge[0]]
-        x1, y1 = pos[edge[1]]
-
-        # Adjusting the arrow position to not overlap with the node
-        adjust = 0.01  # increase adjust factor for better visibility
-        ax = x0 + (x1 - x0) * adjust
-        ay = y0 + (y1 - y0) * adjust
-
-        fig.add_annotation(
-            x=x1, y=y1,
-            ax=ax, ay=ay,
-            xref="x", yref="y",
-            axref="x", ayref="y",
-            showarrow=True,
-            arrowhead=3, 
-            arrowsize=2, 
-            arrowwidth=2,
-            arrowcolor='DarkSlateGrey' # Use a color that contrasts with the background
-        )
-
-    return fig
+    return temp_file.name
 
 def run(df):
 
@@ -97,8 +107,16 @@ def run(df):
     if st.button('Visualize'):
         with st.spinner('Generating Visualization...'):
             df_filtered = get_targets_user_follows(user_id, data)
-            plot = visualize_targets_user_follows(user_id, df_filtered) 
-            st.plotly_chart(plot) # Display the matplotlib plot
+            temp_file_path = visualize_targets_user_follows(user_id, df_filtered)
+            
+            # Read from the temporary file
+            with open(temp_file_path, 'r', encoding='utf-8') as file:
+                source_code = file.read()
+                components.html(source_code, width=700, height=800)
+
+            # Clean up: delete the temporary file
+            os.remove(temp_file_path)
+            shutil.rmtree('lib')
 
 if __name__ == "__main__":
     run()
